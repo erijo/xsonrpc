19a584da1aad11a9c00b3ef319a079ec7b2d2100
Erik Johansson <erik@ejohansson.se>
2015-04-08 20:58:13 +0200

    build: add install target

--- examples/testserver.cpp
+++ examples/testserver.cpp
@@ -15,7 +15,7 @@
 // along with this library; if not, write to the Free Software Foundation,
 // Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-#include "server.h"
+#include "xsonrpc/server.h"
 
 #include <numeric>
 #include <poll.h>
